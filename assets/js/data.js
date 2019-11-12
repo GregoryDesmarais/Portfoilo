4494c8d9cd3a0daadbf855827925f3af3f226a75
Greg D <gregorydesmarais@gmail.com>
2019-11-12 14:52:03 -0500

    Updating CropCircle description

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,7 +2,7 @@ var projects = {
     project2: {
         name: "CropCircle",
         img: "assets/images/cropCircle.png",
-        description: "CropCircle is a small group collaboration done as a group assignment for the coding bootcamp. It is a means of learning new material, practicing old material and showing off to potential employers the skills and abilities of the group members. CropCircle is a fully functioning forum that allows clients to create a unique username and join loose discussions covering different topics.",
+        description: "CropCircle is a small group collaboration assignment for the coding bootcamp. CropCircle is a fully functioning forum that allows clients to create a unique username and join loose discussions covering different topics.",
         codeURL: "https://github.com/GregoryDesmarais/project2-cropcircle",
         liveURL: "https://project2-cropcircle.herokuapp.com/"
     },
