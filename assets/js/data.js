252f526309e5fc96fb0d1a366d871fe81b15aa5b
Greg D <gregorydesmarais@gmail.com>
2019-11-12 15:05:59 -0500

    Updating projects list.

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,7 +2,7 @@ var projects = {
     project2: {
         name: "CropCircle",
         img: "assets/images/cropCircle.png",
-        description: "CropCircle is a small group collaboration utilizing NodeJS, Express, Handlebars, sequelize ORM, and MySQL. CropCircle is a fully functioning forum that allows clients to create a unique username and join loose discussions covering different topics.",
+        description: "A full-stack project utilizing NodeJS, Express, Handlebars, sequelize ORM, and MySQL. CropCircle is a fully functioning forum that allows clients to create a unique username and join loose discussions covering different topics.",
         codeURL: "https://github.com/GregoryDesmarais/project2-cropcircle",
         liveURL: "https://project2-cropcircle.herokuapp.com/"
     },
@@ -26,12 +26,12 @@ var projects = {
         description: "A node.js command-line storefront application.  A customer-oriented app for purchasing, and a manager-oriented app for manipulating the database.",
         codeURL: "https://github.com/GregoryDesmarais/bamazon"
     },
-    LIRI: {
-        name: "LIRI Bot",
-        img: "assets/images/liri.png",
-        description: `A node.js command-line application, which accepts user input to search for music on Spotify, concert venues for bands, or movie information.`,
-        codeURL: "https://github.com/GregoryDesmarais/LIRI-Bot"
-    },
+    // LIRI: {
+    //     name: "LIRI Bot",
+    //     img: "assets/images/liri.png",
+    //     description: `A node.js command-line application, which accepts user input to search for music on Spotify, concert venues for bands, or movie information.`,
+    //     codeURL: "https://github.com/GregoryDesmarais/LIRI-Bot"
+    // },
     FlightPlan: {
         name: "FlightPlan",
         img: "assets/images/plane.gif",
@@ -39,39 +39,39 @@ var projects = {
         codeURL: "https://github.com/GregoryDesmarais/FlightPlan",
         liveURL: "https://gregorydesmarais.github.io/FlightPlan/"
     },
-    wordGuess: {
-        name: "Word Guess",
-        img: "assets/images/code.jpg",
-        description: `A web-development themed "Hangman"-style guessing game.`,
-        codeURL: "https://github.com/GregoryDesmarais/Word-Guess-Game",
-        liveURL: "http://gregorydesmarais.github.io/Word-Guess-Game"
-    },
-    starWarsRPG: {
-        name: "Star Wars RPG",
-        img: "assets/images/RPG.jpg",
-        description: `Pick from a selection of characters and see if you can defeat them all!`,
-        codeURL: "https://github.com/GregoryDesmarais/unit-4-game",
-        liveURL: "http://gregorydesmarais.github.io/unit-4-game"
-    },
-    gifTastic: {
-        name: "Gif-Tastic!",
-        img: "assets/images/gifs.gif",
-        description: `A simple page which utilizes the Giphy API to display gifs based on a search term.  Allows saving and removing searches.`,
-        codeURL: "https://github.com/GregoryDesmarais/Giftastic",
-        liveURL: "http://gregorydesmarais.github.io/Giftastic"
-    },
-    trainSchedule: {
-        name: "Train Schedule",
-        img: "assets/images/trainSchedule.png",
-        description: `Allows a user to add train information to a table, and will update the next available train time once per minute.`,
-        codeURL: "https://github.com/GregoryDesmarais/trainScheduler",
-        liveURL: "http://gregorydesmarais.github.io/trainScheduler"
-    },
-    triviaGame: {
-        name: "Trivia Game!",
-        img: "assets/images/trivia.png",
-        description: `You have 60 seconds to answer as many questions about classic game shows!`,
-        codeURL: "https://github.com/GregoryDesmarais/TriviaGame",
-        liveURL: "http://gregorydesmarais.github.io/TriviaGame"
-    }
+    // wordGuess: {
+    //     name: "Word Guess",
+    //     img: "assets/images/code.jpg",
+    //     description: `A web-development themed "Hangman"-style guessing game.`,
+    //     codeURL: "https://github.com/GregoryDesmarais/Word-Guess-Game",
+    //     liveURL: "http://gregorydesmarais.github.io/Word-Guess-Game"
+    // },
+    // starWarsRPG: {
+    //     name: "Star Wars RPG",
+    //     img: "assets/images/RPG.jpg",
+    //     description: `Pick from a selection of characters and see if you can defeat them all!`,
+    //     codeURL: "https://github.com/GregoryDesmarais/unit-4-game",
+    //     liveURL: "http://gregorydesmarais.github.io/unit-4-game"
+    // },
+    // gifTastic: {
+    //     name: "Gif-Tastic!",
+    //     img: "assets/images/gifs.gif",
+    //     description: `A simple page which utilizes the Giphy API to display gifs based on a search term.  Allows saving and removing searches.`,
+    //     codeURL: "https://github.com/GregoryDesmarais/Giftastic",
+    //     liveURL: "http://gregorydesmarais.github.io/Giftastic"
+    // },
+    // trainSchedule: {
+    //     name: "Train Schedule",
+    //     img: "assets/images/trainSchedule.png",
+    //     description: `Allows a user to add train information to a table, and will update the next available train time once per minute.`,
+    //     codeURL: "https://github.com/GregoryDesmarais/trainScheduler",
+    //     liveURL: "http://gregorydesmarais.github.io/trainScheduler"
+    // },
+    // triviaGame: {
+    //     name: "Trivia Game!",
+    //     img: "assets/images/trivia.png",
+    //     description: `You have 60 seconds to answer as many questions about classic game shows!`,
+    //     codeURL: "https://github.com/GregoryDesmarais/TriviaGame",
+    //     liveURL: "http://gregorydesmarais.github.io/TriviaGame"
+    // }
 }
